7c94290f8d558c8f4f119f467791ce0534819402
Antonio Trujillo <antonio.trujillo01@gmail.com>
2018-03-14 07:00:08 -0600

    Creacion del proyecto

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LefarcWS.Global" Language="C#" %>
---
+++ Global.asax.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using log4net;
+using log4net.Config;
+
+namespace LefarcWS
+{
+    public class Global : System.Web.HttpApplication
+    {
+        private static ILog log = LogManager.GetLogger(typeof(Global));
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            XmlConfigurator.Configure();
+
+            log.Info("Interfases ANTARA-Lefarc Iniciadas");
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
